d4bdc5de102922761ac4bd057f78098cca86743e
Sebastien Levy <m374-crypt0@pm.me>
2026-02-03 15:23:43 +0000

    feat: contracts
    scaffolding

---
+++ contracts/script/LocalDeploy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.33;
+
+import { Script } from "forge-std/Script.sol";
+import { Vm } from "forge-std/Vm.sol";
+
+contract LocalDeployScript is Script {
+  address private sender;
+  address private commitmentStoreOwner;
+
+  function setUp() public {
+    sender = vm.envAddress("TEST_SENDER_ADDRESS");
+    commitmentStoreOwner = vm.addr(uint256(vm.envBytes32("TEST_PRIVATE_KEY_01")));
+  }
+
+  function run() public { }
+
+  // NOTE: To mute uncovered items in coverage reports
+  function test() private { }
+}
---
+++ contracts/test/CommitmentStore.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.33;
+
+import { Test } from "forge-std/Test.sol";
+
+contract CommitmentStoreTests is Test {
+  function setUp() public { }
+
+  function test_unimplementedTest_fails() public pure {
+    assertTrue(false);
+  }
+}
