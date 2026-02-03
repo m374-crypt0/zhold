f725c700f8b6438364a0c69055d28e5b7e6a5bfb
Sebastien Levy <m374-crypt0@pm.me>
2026-02-03 11:04:09 +0000

    feat: customer
    scaffolding with noir and barretenberg dependencies

--- customer/.make/scripts/help.sh
+++ customer/.make/scripts/help.sh
@@ -4,4 +4,7 @@ Usage:
   make <TARGET> where <TARGET> is one of:
 
   - help: print this message
+  - test: test this component
+  - watch: continuously test this component. Run test suites for each file
+    modification
 EOF
---
+++ customer/test/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, it, } from "bun:test";
+
+describe('dummy section', () => {
+  it('should just not work', () => {
+    expect(true, 'Test not implemented').toBeFalse()
+  })
+})
