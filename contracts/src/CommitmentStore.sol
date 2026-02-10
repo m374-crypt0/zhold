aa75babe5994fc5c74aa91103864167e156031eb
Sebastien Levy <m374-crypt0@pm.me>
2026-02-10 10:30:53 +0000

    test: contracts - CommitmentStore
    commitment store first tests
    refactoring openzeppelin
    before working with verifier interfacing

--- contracts/src/CommitmentStore.sol
+++ contracts/src/CommitmentStore.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
-contract CommitmentStore { }
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CommitmentStore is Ownable {
+  error InvalidZeroCommitment();
+  error DuplicateCommitment();
+
+  mapping(uint256 => bool) public commitments;
+
+  event CommitmentStored(uint256 commitment_);
+
+  constructor(address owner_) Ownable(owner_) { }
+
+  function commit(uint256 commitment_) external onlyOwner {
+    require(commitment_ != 0, InvalidZeroCommitment());
+    require(commitments[commitment_] == false, DuplicateCommitment());
+
+    commitments[commitment_] = true;
+
+    emit CommitmentStored(commitment_);
+  }
+}
