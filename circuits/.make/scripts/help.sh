5ef528591dbc92a63c49a26c3f6017a8090f83b8
Sebastien Levy <m374-crypt0@pm.me>
2026-02-10 11:10:17 +0000

    docs: circuits
    make help

--- circuits/.make/scripts/help.sh
+++ circuits/.make/scripts/help.sh
@@ -3,8 +3,10 @@ Usage:
 
   make <TARGET> where <TARGET> is one of:
 
-  - help: print this message
-  - clean: remove compiled artifacts from target directory
-  - compile: compile this circuit
-  - test: test this circuit
+  - help: prints this message
+  - clean: removes compiled artifacts from target directory
+  - compile: compiles this circuit
+  - generate_solidity_verifier: generate a Verifier.sol smart contract in the
+  - test: tests this circuit
+  - watch: tests continuously this circuit for each file change
 EOF
