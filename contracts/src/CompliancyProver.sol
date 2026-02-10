0ff6283879608bad4ba2d03ab633cd27b5f386b7
Sebastien Levy <m374-crypt0@pm.me>
2026-02-10 15:12:28 +0000

    test: contracts - CompliancyProver
    prove, unit tested

--- contracts/src/CompliancyProver.sol
+++ contracts/src/CompliancyProver.sol
@@ -6,5 +6,24 @@ import { IVerifier } from "./interfaces/IVerifier.sol";
 import { CommitmentStore } from "./CommitmentStore.sol";
 
 contract CompliancyProver {
-  constructor(IVerifier verifier_, CommitmentStore store_) { }
+  error InvalidCommitment();
+  error InvalidProof();
+
+  CommitmentStore store;
+  IVerifier verifier;
+
+  constructor(IVerifier verifier_, CommitmentStore store_) {
+    store = store_;
+    verifier = verifier_;
+  }
+
+  function prove(bytes calldata zkp_, bytes32[] calldata publicInputs_) external view returns (bool) {
+    // NOTE: see ../../circuits/src/main.nr, function main arguments
+    uint256 commitment = uint256(publicInputs_[6]);
+
+    require(store.commitments(commitment), InvalidCommitment());
+    require(verifier.verify(zkp_, publicInputs_), InvalidProof());
+
+    return true;
+  }
 }
