9a0bda751e1fcfc44148e8ae2d969f186558d7ec
Sebastien Levy <m374-crypt0@pm.me>
2026-02-13 13:52:02 +0000

    test: issuer
    refuse registration for missing paramters

--- issuer/src/index.ts
+++ issuer/src/index.ts
@@ -1,5 +1,19 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
+  .post("/register", (c) => {
+    return c.json({
+      error: 'missing properties in json object argument',
+      requiredProperties: ['firstName', 'lastName', 'email']
+    }, 400)
+  })
 
+Bun.serve({
+  fetch(req) {
+    return app.fetch(req)
+  },
+  port: 3000
+})
+
+// NOTE: exported for test suites
 export default app;
--- issuer/test/index.test.ts
+++ issuer/test/index.test.ts
@@ -1,12 +1,23 @@
 import { describe, it, expect } from 'bun:test'
+import { testClient } from 'hono/testing'
 
-import app from '../src/'
+import app from '../src/index'
 
 describe('Prospect registration', () => {
-  it('should fail if no data is passed', async () => {
-    const req = new Request('http://localhost:3000/register')
-    const res = await app.fetch(req)
+  const client = testClient(app)
 
-    expect(res.status).toBe(400)
+  it('should fail if arguments are missing', async () => {
+    const res = await client.register.$post()
+
+    const status = res.status;
+    const data = await res.json()
+
+    expect(status).toBe(400)
+
+    expect(data).toContainKey('error')
+    expect(data.error).toBe('missing properties in json object argument')
+
+    expect(data).toContainKey('requiredProperties')
+    expect(data.requiredProperties).toContainAllValues(['firstName', 'lastName', 'email'])
   })
 })
