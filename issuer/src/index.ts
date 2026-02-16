f647fe7dd2354b37eef00ab35e92bca2a05bb55f
Sebastien Levy <m374-crypt0@pm.me>
2026-02-16 12:05:45 +0000

    test: issuer - registration
    checking input

--- issuer/src/index.ts
+++ issuer/src/index.ts
@@ -1,11 +1,32 @@
 import { Hono } from 'hono'
 
+export type RegisterInput = {
+  firstName: string,
+  lastName: string,
+  email: string
+}
+
+export type RegisterOutput = {
+  error: string,
+  requiredProperties: [string] | undefined
+}
+
 const app = new Hono()
-  .post("/register", (c) => {
-    return c.json({
-      error: 'missing properties in json object argument',
-      requiredProperties: ['firstName', 'lastName', 'email']
-    }, 400)
+  .post("/register", async (c) => {
+    try {
+      const args = await c.req.json() as RegisterInput
+
+      if (!args.firstName || !args.lastName || !args.email) {
+        return c.json({
+          error: 'firstName, lastName and email must not be empty',
+        }, 400)
+      }
+    } catch (e) {
+      return c.json({
+        error: 'missing properties in json object argument',
+        requiredProperties: ['firstName', 'lastName', 'email']
+      }, 400)
+    }
   })
 
 Bun.serve({
--- issuer/test/index.test.ts
+++ issuer/test/index.test.ts
@@ -1,16 +1,18 @@
 import { describe, it, expect } from 'bun:test'
 import { testClient } from 'hono/testing'
 
+import { type RegisterInput, type RegisterOutput } from '../src/index'
 import app from '../src/index'
 
 describe('Prospect registration', () => {
   const client = testClient(app)
 
-  it('should fail if arguments are missing', async () => {
-    const res = await client.register.$post(undefined)
+
+  it.each([undefined, null])('should fail if arguments are missing', async (args) => {
+    const res = await client.register.$post({ json: args })
 
     const status = res.status;
-    const data = await res.json()
+    const data = await res.json() as RegisterOutput
 
     expect(status).toBe(400)
 
@@ -19,5 +21,21 @@ describe('Prospect registration', () => {
 
     expect(data).toContainKey('requiredProperties')
     expect(data.requiredProperties).toContainAllValues(['firstName', 'lastName', 'email'])
+  })
+
+  it.each([{ firstName: '', lastName: '', email: '' },
+  { firstName: 'john', lastName: '', email: '' },
+  { firstName: '', lastName: 'doe', email: '' },
+  { firstName: '', lastName: '', email: 'john.doe@unknown.com' },
+  ])('should fail if any required property is empty', async (args) => {
+    const res = await client.register.$post({ json: args as RegisterInput })
+
+    const status = res.status;
+    const data = await res.json() as RegisterOutput
+
+    expect(status).toBe(400)
+
+    expect(data).toContainKey('error')
+    expect(data.error).toBe('firstName, lastName and email must not be empty')
   })
 })
