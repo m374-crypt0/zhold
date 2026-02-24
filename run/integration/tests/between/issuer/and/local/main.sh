4354bfe0da5b501ff99a98233d0d89e93f9498f5
Sebastien Levy <m374-crypt0@pm.me>
2026-02-24 09:45:55 +0000

    test(integration): contracts deployment on local blockchain

--- run/integration/tests/between/issuer/and/local/main.sh
+++ run/integration/tests/between/issuer/and/local/main.sh
@@ -1,5 +1,23 @@
+reset_local_blockchain() {
+  make -C "${RAKE_ROOT_DIR}" contracts kill_local_blockchain 1>/dev/null
+}
+
+deploy_contracts() {
+  make -C "${RAKE_ROOT_DIR}" contracts local_deploy 1>/dev/null 2>&1
+}
+
+deploy_contracts_on_local_blockchain() {
+  reset_local_blockchain &&
+    deploy_contracts
+}
+
+run_integration_tests() {
+  :
+}
+
 main() {
-  echo 'running integration test suite'
+  deploy_contracts_on_local_blockchain &&
+    run_integration_tests
 }
 
 main
