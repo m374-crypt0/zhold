4ece9be8e15a18871072bed2f00a98b78152d93a
Sebastien Levy <m374-crypt0@pm.me>
2026-03-07 13:54:04 +0000

    test(customer): integration test harness
    created, no test yet

---
+++ run/integration/tests/between/customer/and/local/main.sh
@@ -0,0 +1,27 @@
+reset_local_blockchain() {
+  make -C "${RAKE_ROOT_DIR}" contracts kill_local_blockchain 1>/dev/null 2>&1
+}
+
+deploy_contracts() {
+  make -C "${RAKE_ROOT_DIR}" contracts local_deploy 1>/dev/null 2>&1
+}
+
+deploy_contracts_on_local_blockchain() {
+  reset_local_blockchain &&
+    deploy_contracts
+}
+
+run_integration_tests() {
+  if [ "$WATCHING" = 'true' ]; then
+    make -C "${RAKE_ROOT_DIR}" customer pattern=integration watch
+  else
+    make -C "${RAKE_ROOT_DIR}" customer pattern=integration test
+  fi
+}
+
+main() {
+  deploy_contracts_on_local_blockchain &&
+    run_integration_tests
+}
+
+main
