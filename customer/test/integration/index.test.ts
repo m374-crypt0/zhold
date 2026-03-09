a2e8cfd8931e84446a78f44dc9b4e4f67f98263b
Sebastien Levy <m374-crypt0@pm.me>
2026-03-09 12:21:38 +0000

    test(customer): integration
    validating zkp with correct set of public inputs

--- customer/test/integration/index.test.ts
+++ customer/test/integration/index.test.ts
@@ -11,6 +11,9 @@ import { describe, expect, it } from "bun:test";
 const should = '<integration> should'
 
 describe('Proof submission to blockchain', () => {
+  // NOTE: make sure several registration can be done in the same run
+  let emailSuffix = 0
+
   it(`${should} fail to prove on-chain with an unexisting commitment`, async () => {
     const { proof, publicInputs } = await getValidProofAndPublicInputs();
     expect(await customer.verifyProofLocally({ proof, publicInputs })).toBeTrue()
@@ -58,62 +61,97 @@ describe('Proof submission to blockchain', () => {
       publicInputs
     })).toThrow('InvalidProof')
   })
-})
 
-async function recordCompliancyUsingIssuerApi(options: {
-  customerId: number,
-  policy: {
-    id: number,
-    scope: {
-      id: number,
-      parameters: Record<string, string | number | boolean>
+  it(`${should} succeeds to prove on-chain with a valid public input set`, async () => {
+    const privateInputs: Parameters<typeof getValidProofAndPublicInputs>[0] = {
+      customer_id: (await registerUserUsingIssuerApi()).toString(),
+      customer_secret: createCustomerSecret(),
+      authorized_sender: privateKeyToAccount(process.env['TEST_PRIVATE_KEY_03']! as `0x${string}`).address
     }
-  },
-  commitment: string
-}) {
-  const headers = new Headers()
-  headers.append('Content-Type', 'application/json')
 
-  const body = JSON.stringify({
-    customerId: options.customerId,
-    policy: options.policy,
-    commitment: options.commitment
+    const { proof, publicInputs } = await getValidProofAndPublicInputs(privateInputs);
+    expect(await customer.verifyProofLocally({ proof, publicInputs })).toBeTrue()
+
+    await recordCompliancyUsingIssuerApi({
+      customerId: Number(privateInputs.customer_id),
+      policy: {
+        id: Number(publicInputs.policy.id),
+        scope: {
+          id: Number(publicInputs.policy.scope.id),
+          parameters: {
+            validUntil: Number(publicInputs.policy.scope.parameters.valid_until)
+          }
+        }
+      },
+      commitment: `0x${BigInt(publicInputs.request.commitment).toString(16)}`
+    })
+
+    const onChainProver: OnChainProver = new LocalOnChainProver()
+
+    const result = await customer.verifyProofOnChain({
+      onChainProver,
+      proof,
+      publicInputs
+    })
+
+    expect(result).toBeTrue()
   })
 
-  const request = new Request({
-    url: 'http://localhost:3000/customers/recordCompliancy',
-    method: 'POST',
-    headers,
-    body
-  })
+  async function registerUserUsingIssuerApi() {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
 
-  const response = await fetch(request)
+    const body = JSON.stringify({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: `john.doe+${emailSuffix++}@unknown.ufo`
+    })
 
-  const { result } = await response.json() as { result: boolean }
+    const request = new Request({
+      url: 'http://localhost:3000/prospects/register',
+      method: 'POST',
+      headers,
+      body
+    })
 
-  return result
-}
+    const response = await fetch(request)
 
-async function registerUserUsingIssuerApi() {
-  const headers = new Headers()
-  headers.append('Content-Type', 'application/json')
+    const { id } = await response.json() as { id: number }
 
-  const body = JSON.stringify({
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@unknown.ufo'
-  })
+    return id
+  }
 
-  const request = new Request({
-    url: 'http://localhost:3000/prospects/register',
-    method: 'POST',
-    headers,
-    body
-  })
+  async function recordCompliancyUsingIssuerApi(options: {
+    customerId: number,
+    policy: {
+      id: number,
+      scope: {
+        id: number,
+        parameters: Record<string, string | number | boolean>
+      }
+    },
+    commitment: string
+  }) {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
 
-  const response = await fetch(request)
+    const body = JSON.stringify({
+      customerId: options.customerId,
+      policy: options.policy,
+      commitment: options.commitment
+    })
 
-  const { id } = await response.json() as { id: number }
+    const request = new Request({
+      url: 'http://localhost:3000/customers/recordCompliancy',
+      method: 'POST',
+      headers,
+      body
+    })
 
-  return id
-}
+    const response = await fetch(request)
+
+    const { result } = await response.json() as { result: boolean }
+
+    return result
+  }
+})
